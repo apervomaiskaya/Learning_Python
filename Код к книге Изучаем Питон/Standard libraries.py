c85d50b214e636b412d02a8fab7a02eea0e03af4
okrivobokov <apervomaiskaya@ya.ru>
2020-04-09 16:23:41 +1000

    Начал изучать стандартные библиотеки 9-ое апр. 20 г.

--- Код к книге Изучаем Питон/Standard libraries.py
+++ Код к книге Изучаем Питон/Standard libraries.py
@@ -19,3 +19,48 @@ die1.roll_die(6)
 die1.roll_die(10)
 die1.roll_die(20)
 
+lottery = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'а', 'б', 'в', 'г', 'д']
+from random import choice
+rannum = choice(lottery)
+print(str(rannum), str(rannum), str(rannum), str(rannum), str(rannum))
+for i1 in lottery:
+    i1 = choice(lottery)
+for i2 in lottery:
+    i2 = choice(lottery)
+for i3 in lottery:
+    i3 = choice(lottery)
+for i4 in lottery:
+    i4 = choice(lottery)
+print([i1, i2, i3, i4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
