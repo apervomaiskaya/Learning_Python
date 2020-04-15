fdd97b2dafbb60d20325396702509c755aa7ead0
okrivobokov <apervomaiskaya@ya.ru>
2020-04-15 15:32:39 +1000

    Начал изучать исключения 15-ое апр. 20 г.

--- Код к книге Изучаем Питон/Exceptions.py
+++ Код к книге Изучаем Питон/Exceptions.py
@@ -2,3 +2,22 @@ try:
     print(5/0)
 except ZeroDivisionError:
     print('Вы не можете делить на ноль!')
+
+print()
+
+print("Дайте мне два числа, и я разделю их.")
+print('Напишите "выход", чтобы выйти.')
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print('Вы не можете делить на ноль!')
+    else:
+        print(answer)
