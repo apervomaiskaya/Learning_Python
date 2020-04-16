80587294b6c5ba5178046f5e0d3e644965e257f4
okrivobokov <apervomaiskaya@ya.ru>
2020-04-16 14:54:08 +1000

    # ЧАСТОТНОСТЬ СЛОВА "the" Продолжал изучать исключения 16-ое апр. 20 г.

--- Код к книге Изучаем Питон/Exceptions.py
+++ Код к книге Изучаем Питон/Exceptions.py
@@ -64,6 +64,27 @@ for filename in filenames:
     except FileNotFoundError:
         print('Файл', filename, 'не найден')
 
+# ЧАСТОТНОСТЬ СЛОВА "the"
+filename = 'Meine erste Weltreise.txt'
+try:
+    with open(filename, encoding='utf-8') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Sorry, the file {filename} does not exist.")
+else:
+    # Подсчет приблизительного количества строк в файле.
+    words = contents.split()
+    num_words = len(words)
+    line1 = contents.count('the ')
+    line2 = contents.count('the')
+    print('Количество слова "the " равно: ', line1)
+    print('Количество слова "the" равно: ', line2)
+    print(f"The file {filename} has about {num_words} words.")
+    print(int(line2) - int(line1))
+
+
+
+
 
 
 
