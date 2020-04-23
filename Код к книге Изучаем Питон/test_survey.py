aebd0cf0e0b801cb486d54ea01b43ff789a856b0
okrivobokov <apervomaiskaya@ya.ru>
2020-04-23 16:34:27 +1000

    Продолжал изучать тестирование 23-ье апр. 20 г.

--- Код к книге Изучаем Питон/test_survey.py
+++ Код к книге Изучаем Питон/test_survey.py
@@ -6,10 +6,21 @@ class TestAnonymousSurvey(unittest.TestCase):
 
     def test_store_single_response(self):
         #Проверяет, что один ответ сохранён правильно.
-        question = "Какой у вас первый язык?"
+        question = 'Какой у вас первый язык?'
         my_survey = AnonymousSurvey(question)
         my_survey.store_response('русский')
         self.assertIn('русский', my_survey.responses)
+
+    def test_store_three_responses(self):
+        #Проверяет, что 3 ответа были сохранены правильно
+        question = 'Какой у вас первый язык?'
+        my_survey = AnonymousSurvey(question)
+        responses = ['русский', 'англ', 'китай']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
 
 if __name__ == '__main__':
     unittest.main()
