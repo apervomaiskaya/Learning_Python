ebc9729240dbf8b58f4067b4000f04f4dc2e41f0
okrivobokov <apervomaiskaya@ya.ru>
2020-04-23 15:19:49 +1000

    Продолжал изучать тестирование 23-ье апр. 20 г.

---
+++ Код к книге Изучаем Питон/language_survey.py
@@ -0,0 +1,17 @@
+from survey import AnonymousSurvey
+
+# Определение вопроса с созданием экземпляра AnonymousSurvey.
+question = "Какой язык ты выучил первым, чтобы говорить?"
+my_survey = AnonymousSurvey(question)
+# Вывод вопроса и сохранение ответов.
+my_survey.show_question()
+print("Напишите 'в', чтобы выйти.\n")
+while True:
+    response = input("Язык: ")
+    if response == 'в':
+        break
+    my_survey.store_response(response)
+
+# Вывод результатов опроса.
+print("\nСпасибо всем, кто поучаствовал в опросе!")
+my_survey.show_results()
