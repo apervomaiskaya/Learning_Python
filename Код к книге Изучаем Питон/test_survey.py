84fae672ae3cedd6e456bfb2ae11c32225468414
okrivobokov <onxu@ya.ru>
2020-04-24 15:20:16 +1000

    Продолжал изучать тестирование 24-ое апр. 20 г.

--- Код к книге Изучаем Питон/test_survey.py
+++ Код к книге Изучаем Питон/test_survey.py
@@ -4,28 +4,29 @@ from survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
     #Тесты для класса AnonymousSurvey
 
-    def test_store_single_response(self):
-        #Проверяет, что один ответ сохранён правильно.
+    def setUp(self):
+        # Создание опроса и набора ответов
+        # для всех тестовых медотодов
         question = 'Какой у вас первый язык?'
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('русский')
-        self.assertIn('русский', my_survey.responses)
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['русский', 'англ', 'китай']
+
+    def test_store_single_response(self):
+        # Проверяет, что один ответ сохранён правильно.
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
     def test_store_three_responses(self):
-        #Проверяет, что 3 ответа были сохранены правильно
-        question = 'Какой у вас первый язык?'
-        my_survey = AnonymousSurvey(question)
-        responses = ['русский', 'англ', 'китай']
+        # Проверяет, что 3 ответа были сохранены правильно
         for response in responses:
-            my_survey.store_response(response)
+            self.my_survey.store_response(response)
 
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 if __name__ == '__main__':
     unittest.main()
 
-print()
 
 
 
